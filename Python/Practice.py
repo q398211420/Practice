01aad6d5285084dbf1bdb9d66f0aa466f2389e2c
Sammy <40032186+q398211420@users.noreply.github.com>
2024-10-13 12:27:17 +0800

    目录更名

--- Python/Practice.py
+++ Python/Practice.py
@@ -1,9 +1,1 @@
 args = input()
-match args:
-    case ['gcc']:
-        print('missing file')
-    case ['gcc', file1, *file2]
-        print('gcc ' + file1 + ' '.join(file2))
-    case _:
-        print('invalid arguments')
-    
--- src/ServerAndClient.cpp
+++ src/ServerAndClient.cpp
@@ -4,7 +4,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define SERVER
+#define CLIENT
 #ifdef SERVER
 int main(int argc, const char** argv)
 {
@@ -52,7 +52,7 @@ int main(int argc, const char** argv)
 
     while (1) {
         int new_socket;
-
+        char buffer[1024] = {0};
         // struct sockaddr_in client_addr;
         // socklen_t client_len = sizeof(client_addr);
         // 这里是重用的之前的sockaddr_in变量，没有新定义
@@ -66,12 +66,24 @@ int main(int argc, const char** argv)
 
         // 打印连接信息和进程ID
         printf("New connection accepted from %s:%d, PID: %d\n",
-               inet_ntoa(address.sin_addr), 
-               
+               inet_ntoa(address.sin_addr),
+
                (address.sin_port), pid);
 
         // 处理连接
         // 在这里可以执行与客户端通信的代码
+        int bytes_received = recv(new_socket, buffer, sizeof(buffer), 0);
+
+        if (bytes_received == -1) {
+            std::cerr << "recv failed" << std::endl;
+        }
+        else if (bytes_received == 0) {
+            std::cout << "Connection closed by the peer" << std::endl;
+        }
+        else {
+            std::cout << "Received: " << std::string(buffer, bytes_received)
+                      << std::endl;
+        }
 
         // 关闭连接
         close(new_socket);
@@ -85,9 +97,55 @@ int main(int argc, const char** argv)
 #endif
 
 #ifdef CLIENT
-int main(int argc, const char** argv)
+#    include <iostream>
+#    include <cstring>
+#    include <sys/socket.h>
+#    include <arpa/inet.h>
+#    include <unistd.h>
 
+int main()
+{
+    int sock = 0;
+    struct sockaddr_in server_address;
+    std::cout << "please enter content:" << std::endl;
+    std::string message;
+    std::cin >> message;
+    char buffer[1024] = {0};  // 用于接收服务器的响应
 
+    // 创建套接字 (AF_INET = IPv4, SOCK_STREAM = TCP)
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Client socket creation error" << std::endl;
+        return -1;
+    }
+
+    // 设置服务器地址结构
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);  // 服务器的端口，确保与服务器一致
+
+    // 将 IPv4 地址从文本转换为二进制形式
+    if (inet_pton(AF_INET, "162.14.76.14", &server_address.sin_addr) <= 0) {
+        std::cerr << "Invalid address / Address not supported" << std::endl;
+        return -1;
+    }
+
+    // 尝试连接到服务器
+    if (connect(sock, (struct sockaddr*)&server_address,
+                sizeof(server_address)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+
+    // 发送消息到服务器
+    send(sock, message.c_str(), strlen(message.c_str()), 0);
+    std::cout << "Message sent: " << message << std::endl;
+
+    // 接收服务器的响应
+    int valread = recv(sock, buffer, sizeof(buffer),0);
+    std::cout << "Response from server: " << std::string(buffer, valread)
+              << std::endl;
+
+    // 关闭套接字
+    close(sock);
     return 0;
 }
 #endif
